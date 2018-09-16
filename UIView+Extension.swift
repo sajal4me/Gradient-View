53699db44f34e407ca6c59dc7387a192cd0756e9
Sajal <sajalgupta4me@gmail.com>
2018-09-16 17:39:01 +0530

    Added gradient code

---
+++ UIView+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Extension.swift
+//
+//  Created by Sajal Gupta on 16/08/18.
+//  Copyright © 2017 Sajal Gupta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
